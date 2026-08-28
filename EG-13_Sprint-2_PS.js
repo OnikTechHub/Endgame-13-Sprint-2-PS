2855518b76c0f1c899abe63d22c07c9e6803dd1f
onik-999 <onikdas.dev@gmail.com>
2026-08-28 20:21:30 +0600

    feat: add solution for problem 4 (Sum Array Elements)

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -60,3 +60,24 @@ function isPalindrome(str){
 }
 
 console.log(isPalindrome("racecar")); 
+
+
+
+
+// 4. Sum Array Elements
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function sumArray(nums){
+    let sum = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    
+    return sum;
+}
+
+console.log(sumArray([10, 20, 30, 40]));
