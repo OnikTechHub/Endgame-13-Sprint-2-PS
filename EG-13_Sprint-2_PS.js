b3daa55097b759e6091d68bf463e032e99c5e149
onik-999 <onikdas.dev@gmail.com>
2026-08-28 20:08:47 +0600

    feat: add solution for problem 1 (Reverse a String)

---
+++ EG-13_Sprint-2_PS.js
@@ -0,0 +1,17 @@
+// 1. Reverse a String
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function reverseString(str){
+    let reversedStr = "";
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    
+    return reversedStr;
+}
+
+console.log(reverseString("hello"));
